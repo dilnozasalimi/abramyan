6f74d068678ece138240083c54dcf8e1e0f39637
Dilnoza Salimi <salimidilnoza1307@gmail.com>
2022-01-27 18:16:35 +0500

    updated begin 1

--- begin/1.js
+++ begin/1.js
@@ -1,3 +1,9 @@
-const a=4;
-const p=4*a;
-console.log(p);
+function perimetr(a) {
+    const p=4*a;
+    return p;
+}
+
+const inputText = window.prompt('Please inter value for a', 0);
+
+const result=perimetr(inputText);
+alert(result);
---
+++ begin/6.js
@@ -0,0 +1,7 @@
+const a=3;
+const b=2;
+const c=4;
+const v=a*b*c;
+const s=2*(a*b+b*c+a*c);
+console.log(v);
+console.log(s);
