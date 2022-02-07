ebe6cb18f51258916748928841ea444c2ae12d04
Dilnoza Salimi <salimidilnoza1307@gmail.com>
2022-02-07 17:45:28 +0500

    resolved integer 19

---
+++ integer/19.js
@@ -0,0 +1,8 @@
+function time(A) {
+    A = parseInt(A);
+    const t=A/100;
+    return t;
+  }
+  const inputA = window.prompt('please enter value for A', 0);
+  const result = time(inputA);
+  alert(result);
